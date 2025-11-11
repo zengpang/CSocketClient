705aca49409ac65e2854b1896a05f4a4d944a8db
曾胖 <2516629927@qq.com>
2025-11-11 21:27:03 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <iostream>
-
+#include <string>
+#pragma comment(lib,"Ws2_32.lib")
+#define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
 int main(int, char**){
+    WSADATA wsaData;
+    int iResult;
+    iResult=WSAStartup(MAKEWORD(2,2),&wsaData);
+    if(iResult!=0)
+    {
+        std::cerr<<"WSAStartup failed: "<<iResult<<std::endl;
+        return 1;
+    }
+    struct addrinfo *result=NULL,*ptr=NULL,hints;
+    ZeroMemory(&hints,sizeof(hints));
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype=SOCK_STREAM;
+    hints.ai_protocol=IPPROTO_TCP;
     std::cout << "Hello, from CSocketClient!\n";
 }
