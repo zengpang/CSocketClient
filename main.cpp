d35b17bc22a92aad746dae61f98e53da8ae2ce7d
曾胖 <2516629927@qq.com>
2025-11-13 20:58:53 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #pragma comment(lib, "Ws2_32.lib")
-#define DEFAULT_PORT "27015"
+#define DEFAULT_PORT "8888"
 #define DEFAULT_BUFLEN 512
 int main(int, char **)
 {
